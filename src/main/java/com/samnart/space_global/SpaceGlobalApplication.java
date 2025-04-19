5d0fca6535ebdcb39071bd84f6350d8fb0033ea7
samnart1 <originalsamnart@gmail.com>
2025-04-19 16:16:46 +0200

    Initial commit

---
+++ src/main/java/com/samnart/space_global/SpaceGlobalApplication.java
@@ -0,0 +1,13 @@
+package com.samnart.space_global;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceGlobalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceGlobalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samnart/space_global/SpaceGlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samnart.space_global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceGlobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
